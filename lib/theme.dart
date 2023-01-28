47dc4b4ad3bd7b402fd0408a4bbd5cffc16c80c7
Viraj <virajmalaka94@gmail.com>
2023-01-28 19:55:55 +0530

    created theme.dart

---
+++ lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MyTheme {
+  static ThemeData darkTheme = ThemeData(
+    brightness: Brightness.dark,
+    primarySwatch: Colors.yellow,
+    fontFamily: 'GemunuLibre',
+  );
+  static ThemeData lightTheme = ThemeData(
+    brightness: Brightness.light,
+    primarySwatch: Colors.yellow,
+    fontFamily: 'GemunuLibre',
+  );
+}
